a46f3f7656e5c494b7ffc776d78c05f5e3392b4b
Timofey S <timsolodev@gmail.com>
2022-05-21 21:13:37 +0300

    feat: publish event to PgQ queue `outbox`

---
+++ app/infrastructure/repository/postgres/publish_event.go
@@ -0,0 +1,29 @@
+package postgres
+
+import (
+	"context"
+	"ms-users/app/domain/entity"
+)
+
+// Publish saves to PgQ an event.
+// This function can't be tested properly.
+func (d *DB) Publish(ctx context.Context, subject string, payload []byte) error {
+	row, err := d.one(ctx, "SELECT pgq.insert_event('outbox', ?, ?)", subject, payload)
+	if err != nil {
+		return err
+	}
+
+	// the insert_event function should returns Event ID
+	// see: https://pgq.github.io/extension/pgq/files/external-sql.html#pgq.insert_event(3)
+	var r int64
+	err = row.Scan(&r)
+	if err != nil {
+		return E(err)
+	}
+
+	if r == 0 {
+		return entity.ErrMismatch
+	}
+
+	return nil
+}
