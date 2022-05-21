f67ecb86bd1fc23711377d84862662dfcf77a3de
Timofey S <timsolodev@gmail.com>
2022-05-21 21:15:14 +0300

    refactor: remove single repository from domain folder

--- README.md
+++ README.md
@@ -14,5 +14,7 @@ http://0.0.0.0/metric/
 
 ## TODO
 [ ] Healthcheck for all dependencies
+    [ ] PostgreSQL
+    [ ] NATS
 [ ] Opentelemetry
 [x] Prometheus
--- app/infrastructure/delivery/cli/migrate.go
+++ app/infrastructure/delivery/cli/migrate.go
@@ -1,18 +1,17 @@
 package cli
 
 import (
-	"ms-users/app/domain/repository"
 	"ms-users/app/infrastructure/logger"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-type migrator interface {
+type Migrator interface {
 	Migrate(migrateCmd string) (stepsDone int, err error)
 }
 
-func NewMigrateCmd(log logger.Logger, r repository.Repository) *cobra.Command {
+func NewMigrateCmd(log logger.Logger, m Migrator) *cobra.Command {
 	return &cobra.Command{
 		Use:   "migrate <up|down|1|-1>",
 		Short: "Execute migration",
@@ -23,10 +22,6 @@ func NewMigrateCmd(log logger.Logger, r repository.Repository) *cobra.Command {
 			"number - amount of steps to migrate (if > 0 - migrate number steps up, if < 0 migrate number steps down)",
 		Args: cobra.ExactArgs(1),
 		Run: func(_ *cobra.Command, args []string) {
-			m, ok := r.(migrator)
-			if !ok {
-				log.Fatalf("repository desn't support migration")
-			}
 			log = log.WithFields(logger.Fields{"module": "migrate"})
 			n, err := m.Migrate(args[0])
 			if err != nil {
